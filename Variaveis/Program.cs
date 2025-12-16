e6a6717aaa46fca0dfffcc543370670c73b0bb57
josueperoli <josue.jpo2003@gmail.com>
2025-12-16 12:27:13 -0300

    Variaveis comentários

--- Variaveis/Program.cs
+++ Variaveis/Program.cs
@@ -1,14 +1,15 @@
-﻿public class Program
+﻿p;ublic class Program
 {
     public static void Main()
     {
         string nome = "Josue";
         int idade = 22;
         double num = 10.34;
+        int todo = 78;
         
-        //var reconhece o tipo de dado que será colocado na variável
+        //var reconhece o tipo de dado que será colocado na variável.
 
         var variavel = "adapatável";
-        Console.WriteLine(nome + idade + num + variavel);
+        Console.WriteLine(nome + idade + num + variavel + todo);
     }
 }
