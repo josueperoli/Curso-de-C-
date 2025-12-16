ae6d9013f0a46b914d2300864db78f876adb5311
josueperoli <josue.jpo2003@gmail.com>
2025-12-16 12:01:08 -0300

    Variaveis

---
+++ Variaveis/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string nome = "Josue";
+        int idade = 22;
+        double num = 10.34;
+        boolean n = true;
+        
+
+        Console.WriteLine(nome);
+    }
+}
