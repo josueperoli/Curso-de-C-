6dc9c620e8233fbfb851a77dc87467adce6a83b8
josueperoli <josue.jpo2003@gmail.com>
2026-01-12 12:07:51 -0300

    Estrutura Condicional

--- Estrutura Condicional/Program.cs
+++ Estrutura Condicional/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite um número: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if(num % 2 == 0){
+            Console.WriteLine("É par!");
+        }
+        else
+        {
+            Console.WriteLine("É ímpar!");
+        }
+    }
+}
