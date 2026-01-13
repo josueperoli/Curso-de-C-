d447c3845ee4a5ef8e8d7a391c9bf491e794fca9
josueperoli <josue.jpo2003@gmail.com>
2026-01-13 14:10:20 -0300

    Operadores de Atribuição

---
+++ Operadores de Atribuição/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        //Inicializar uma variável
+        int x = 10;
+        Console.WriteLine("Valor inicial de x: " + x);
+
+        //Operador de Atribuição Simples 
+        x = 20;
+        Console.WriteLine("Atribuição simples, x vale: " + x);
+
+        //Atribuição Composta (Incremento) (++)
+        x++; //x = x + 1
+        Console.WriteLine("Atribuição Composta Incremento, x vale: " + x);
+
+        //Atribuição Composta (+=)
+        x += 10; //x = x + 10
+        Console.WriteLine("Atribuição Composta, x vale: " + x);
+
+        //Atribuição Composta (-=)
+        x -= 10; //x = x - 10
+        Console.WriteLine("Atribuição Composta, x vale: " + x);
+
+        //Atribuição Composta (*=)
+        x *= 10; //x = x * 10
+        Console.WriteLine("Atribuição Composta, x vale: " + x);
+
+        //Atribuição Composta (/=)
+        x /= 10; //x = x / 10
+        Console.WriteLine("Atribuição Composta, x vale: " + x);
+    }
+}
